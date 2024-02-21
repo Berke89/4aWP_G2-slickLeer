325b0704fbc9cbd8e109a12e4f3d6cd88811d3c6
berke <berke.tuncbeden@student.htldornbirn.at>
2024-02-21 10:19:57 +0100

    First Commit

---
+++ src/at/tub/game/MeinUfo.java
@@ -0,0 +1,32 @@
+package at.tub.game;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.geom.Shape;
+
+public class MeinUfo extends Spielobjekt {
+    public MeinUfo(int x, int y, Image image) {
+        super(x, y, image);
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        this.getImage().drawCentered(this.getX(),this.getY());
+
+    }
+
+    @Override
+    public Shape getShape() {
+        return null;
+    }
+
+    @Override
+    public void update(int delta) {
+        if (this.getY() > (768+this.getHeight())){
+            this.setY(-this.getHeight());
+        }
+        this.setY(this.getY()+4);
+
+
+    }
+}
